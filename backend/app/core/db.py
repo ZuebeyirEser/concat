1f6cc80e6c86c57cde4b8b48b7bd7793af5c4e6f
ZuebeyirEser <zubeyir.eser@icloud.com>
2025-10-21 10:02:44 +0200

    refactor: update main app and database configuration

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -2,7 +2,8 @@ from sqlmodel import Session, create_engine, select
 
 from app import crud
 from app.core.config import settings
-from app.models import User, UserCreate
+from app.models import User
+from app.schemas import UserCreate
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
@@ -30,4 +31,4 @@ def init_db(session: Session) -> None:
             password=settings.FIRST_SUPERUSER_PASSWORD,
             is_superuser=True,
         )
-        user = crud.create_user(session=session, user_create=user_in)
+        user = crud.user.create(session, obj_in=user_in)
